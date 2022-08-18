27c1792b6682021805b41bfbd861200b78afda27
王心膂 <22200099@student.uwa.edu.au>
2022-08-19 02:07:14 +0800

    project2: completed About & Interests & Footer function and css

--- project2/index.js
+++ project2/index.js
@@ -1,29 +1,69 @@
 function Info(){
     return(
-        <div className="Info">
-            <img className="photo" src="./head.jpeg" alt="Rick-photo"></img>
-            <h1>Xinlyu Wang Rick</h1>
-            <h3>Frontend Developer</h3>
-            <p>Master of IT % Master of Professional Engineering</p>
-            <div>
-                <a role="button" href="mailto: xinlyuwang96@gmail.com" className="btn btn-light">Email</a>
-                <a role="button" href="www.linkedin.com/in/xinlyuwang" className="btn btn-primary">LinkedIn</a>
+        <div className="Info container">
+            <div className="row row-content">
+                <div className="col-4"></div>
+                <div className="col-4">
+                        <img className="photo" src="./head.jpeg" alt="Rick-photo"></img>
+                        <h1>Xinlyu Wang Rick</h1>
+                        <h3>Frontend Developer</h3>
+                        <p>Master of IT &amp; Master of Professional Engineering</p>
+                        <div className="btnlink">
+                            <a role="button" href="mailto: xinlyuwang96@gmail.com" className="btn btn-light"><span className="fa-solid fa-envelope"></span><span> Email</span></a>
+                            <a role="button" href="https://www.linkedin.com/in/xinlyuwang" className="btn btn-primary"><span className="fa-brands fa-linkedin"></span><span> LinkedIn</span></a>
+                        </div>
+                </div>
+                <div className="col-4"></div>
             </div>
-            
-
         </div>
     )
 }
 
 function About(){
     return(
-        <h1>About</h1>
+        <div className="About container">
+        <div className="row row-content">
+            <div className="col-4"></div>
+            <div className="aboutcont col-4">
+                <h2>About</h2>
+                <p>Master of IT &amp; Master of Professional Engineering</p>
+            </div>
+            <div className="col-4"></div>
+        </div>
+    </div>
     )
 }
 
 function Interests(){
     return(
-        <h1>Interests</h1>
+        <div className="Interests container">
+        <div className="row row-content">
+            <div className="col-4"></div>
+            <div className="interestscont col-4">
+                <h2>Interests</h2>
+                <p>Master of IT &amp; Master of Professional Engineering</p>
+            </div>
+            <div className="col-4"></div>
+        </div>
+    </div>
+    )
+}
+
+function Footer(){
+    return(
+        <div className="Footer container">
+        <div className="row row-content">
+            <div className="col-3"></div>
+            <footer className="footer"> 
+                <a role="button" href="#" className="footbtn btn-dark"><span className="fa-brands fa-discord"></span></a>
+                <a role="button" href="#" className="footbtn btn-dark"><span className="fa-brands fa-facebook"></span></a>
+                <a role="button" href="#" className="footbtn btn-dark"><span className="fa-brands fa-instagram"></span></a>
+                <a role="button" href="#" className="footbtn btn-dark"><span className="fa-brands fa-twitter"></span></a>
+                <a role="button" href="#" className="footbtn btn-dark"><span className="fa-brands fa-github"></span></a>
+            </footer>
+            <div className="col-3"></div>
+        </div>
+    </div>
     )
 }
 
@@ -34,5 +74,6 @@ root.render(
         <Info />
         <About />
         <Interests />
+        <Footer />
     </div>
 )
