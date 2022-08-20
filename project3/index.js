2d1820b5790127e27c79939bc6a4da5825e523fa
王心膂 <22200099@student.uwa.edu.au>
2022-08-20 15:14:02 +0800

    project3: build up MainContent & Cards function

--- project3/index.js
+++ project3/index.js
@@ -1,28 +1,60 @@
-//import React from "react"
-//import ReactDOM from "react-dom/client"
-//import Header from "./Header"
-//import Footer from "./Footer"
-//import MainContent from "./MainContent"
-
 function Nav() {
     return (
         <nav className="nav">
-            <img src="./images/airbnb-logo.png" className="nav-logo" />
+            <img src="./images/airbnb-logo.png" className="nav-logo" alt="airbnb-logo" />
         </nav>
     )
 }
 
 function MainContent() {
     return (
-        <div>
+        <div className="hero">
+            <div className="hero-img">
+                <img src="./images/photo-grid.png" alt="photo-grid" />
+            </div>
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
+    )
+}
+function Cards() {
+    return (
+        <section>
+            <div className="container text-center">
+                <div className="row align-items-start">
+                    <div className="col-4">
+                        <div className="card">
+                            <img src="./images/katie-zaferes.png" className="card-img-top" alt="katie" />
+                            <div className="card-body">
+                                <div className="card-star">
+                                    <img src="./images/star.png" className="star-img" alt="star" />
+                                    <p>(6)</p>
+                                    <p>.</p>
+                                    <p>USA</p>
+                                </div>
+                                <p>Life lessons with Katie Zaferes</p>
+                                <p><strong>From $136</strong> / person</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+
+                    </div>
+                    <div className="col-4">
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        
     )
 }
 
 function Footer() {
     return (
         <footer>
-        </footer>
+        </footer> 
     )
 }
 
@@ -31,6 +63,7 @@ function Page() {
         <div>
             <Nav />
             <MainContent />
+            <Cards />
             <Footer />
         </div>
     )
